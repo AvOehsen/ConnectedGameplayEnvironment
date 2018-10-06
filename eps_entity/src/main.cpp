052249f6393e326799ed0ff4d2792a4eb7ad5956
Arne von Öhsen <v.oehsen@gmail.com>
2018-10-07 01:07:12 +0200

    ESP can be configured via WEB interface (some issues with connecting to a wlan ... maybe problem in the lib)

--- eps_entity/src/main.cpp
+++ eps_entity/src/main.cpp
@@ -1,17 +1,20 @@
+#include <FS.h>
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ArduinoJson.h>
+#include <WiFiManager.h>
+#include <Esp.h>
+
 
 #include "modules/baseModule.h"
 #include "modules/triggerModule.h"
 
-//TODO
-#define WIFI_SSID ""    
-#define WIFI_PASS ""
-#define SERVER_IP ""
 #define SERVER_PORT 11000
-#define UNIQUE_DEVICE_ID "dummy"
+
+char device_id[30] = "uninitialized_esp_device";
+char server_ip[16] = "ip_not_set";
+bool saveConfig = false;
 
 WiFiClient _client;
 DynamicJsonBuffer _buffer(512);
@@ -20,7 +23,6 @@ String _stringBuffer;
 const int _modulesCount = 3;
 AbstractBaseModule *_modules[_modulesCount];
 
-
 void defineModules()
 {
     _modules[0] = new TriggerModule();
@@ -28,22 +30,121 @@ void defineModules()
     _modules[2] = new TriggerModule();
 }
 
+void switchBuildinLed()
+{
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
+
+void onSaveConfig()
+{
+    saveConfig = true;
+}
+
+void onEnterConfigModus(WiFiManager *mgr)
+{
+    //digitalWrite(LED_BUILTIN, LOW);
+}
+
+void netConfig(bool force)
+{
+    WiFiManager mgr;
+    mgr.setConnectTimeout(30);
+    mgr.setSaveConfigCallback(onSaveConfig);
+    mgr.setAPCallback(onEnterConfigModus);
+
+    if(SPIFFS.begin())
+    {
+        if(SPIFFS.exists("/config.json"))
+        {
+            File configFile = SPIFFS.open("/config.json", "r");
+            JsonObject &config = _buffer.parse(configFile);
+
+            if(config.success())
+            {
+                strcpy(device_id, config["device_id"]);
+                strcpy(server_ip, config["server_ip"]);
+            }
+            else
+                force = true;
+        }
+        else
+            force = true;
+    }
+    else
+        force = true;
+
+    WiFiManagerParameter deviceIdParam("device id", "", device_id, 30);
+    WiFiManagerParameter serverIpParam("server ip", "", server_ip, 16);
+
+    mgr.addParameter(&deviceIdParam);
+    mgr.addParameter(&serverIpParam);
+    
+    if(force)
+        mgr.startConfigPortal(device_id);
+    else
+        mgr.autoConnect(device_id);
+
+    Serial.println("wifi connection success!");
+
+    strcpy(device_id, deviceIdParam.getValue());
+    strcpy(server_ip, serverIpParam.getValue());
+
+    if(saveConfig)
+    {
+        Serial.println("saving config ...");
+
+        JsonObject& json = _buffer.createObject();
+        json["device_id"] = device_id;
+        json["server_ip"] = server_ip;
+
+        File configFile = SPIFFS.open("/config.json", "w");
+        json.printTo(configFile);
+        configFile.close();
+
+        Serial.println("save config success!");
+    }
+}
+
+bool enableConfigReset()
+{
+    uint32_t flag = 0;
+    ESP.rtcUserMemoryRead(0, &flag, sizeof(flag));
+
+    if(flag == 0)
+    {
+        flag = 1;
+        ESP.rtcUserMemoryWrite(0, &flag, sizeof(flag));
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(1000);
+        flag = 0;
+        ESP.rtcUserMemoryWrite(0, &flag, sizeof(flag));
+        digitalWrite(LED_BUILTIN, HIGH);
+
+        return false;
+    }
+    else
+    {
+        flag = 0;
+        ESP.rtcUserMemoryWrite(0, &flag, sizeof(flag));
+        
+        return true;
+    }
+}
+
 void setup() 
 {
+    Serial.begin(9600);
     defineModules();
 
-    Serial.begin(9600);
-    WiFi.begin(WIFI_SSID, WIFI_PASS);
-    while(WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        Serial.print(".");
-    }
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
 
-    Serial.println(" connected to WiFi");
-    Serial.println(WiFi.localIP());
-    
-    if(!_client.connect(SERVER_IP, SERVER_PORT))
+    bool rebuildConfig = enableConfigReset(); 
+    netConfig(rebuildConfig);
+
+    digitalWrite(LED_BUILTIN, HIGH);
+        
+    if(!_client.connect(server_ip, SERVER_PORT))
     {
         Serial.println("connection to server failed!");
     }
@@ -55,7 +156,7 @@ void setup()
 
         JsonObject& obj = _buffer.createObject();
         obj["type"] = "definition";
-        obj["deviceId"] = UNIQUE_DEVICE_ID;
+        obj["deviceId"] = device_id;
         
         JsonArray& array = obj.createNestedArray("modules");
         for(int i = 0; i < _modulesCount; i++)
@@ -68,6 +169,7 @@ void setup()
         obj.printTo(Serial);
         Serial.println();
     }
+
 }
 
 void parseMessage(const char* json)
